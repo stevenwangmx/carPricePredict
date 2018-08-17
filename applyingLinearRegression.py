7044f8fe7389fcc77deed52e34bcbac23ca9d8a6
stevenwangmx <wangmingxi7@Hotmail.com>
2018-08-17 13:41:35 -0400

    Update applyingLinearRegression.py

--- applyingLinearRegression.py
+++ applyingLinearRegression.py
@@ -30,7 +30,7 @@ encoded = ohe.fit(Features.reshape(-1,1))
 Features = encoded.transform(Features.reshape(-1,1)).toarray()
 ## finished
 
-## use function to encode all other features left and append them together
+## Call function to encode all other features left and append them together
 def encode_string(cat_feature):
     ## First encode the strings to numeric categories
     enc = preprocessing.LabelEncoder()
@@ -51,3 +51,90 @@ print(Features.shape)   # display result
 print(Features[:2, :])  # display result  
 ## finished
 
+## Add the numeric features
+Features = np.concatenate([Features, np.array(auto_prices[['curb_weight', 'horsepower', 'city_mpg']])], axis = 1)
+Features[:2,:]
+
+###### Split the data set ######
+## Randomly sample cases to create independent training and test data
+nr.seed(9988)
+labels = np.array(auto_prices['log_price'])
+indx = range(Features.shape[0])
+indx = ms.train_test_split(indx, test_size = 40)
+x_train = Features[indx[0],:]
+y_train = np.ravel(labels[indx[0]])
+x_test = Features[indx[1],:]
+y_test = np.ravel(labels[indx[1]])
+
+###### Rescale numeric features ######
+scaler = preprocessing.StandardScaler().fit(x_train[:,14:])
+x_train[:,14:] = scaler.transform(x_train[:,14:])
+x_test[:,14:] = scaler.transform(x_test[:,14:])
+x_train[:5,:]
+
+###### Construct regression model ######
+## define and fit the linear regression model## defin 
+lin_mod = linear_model.LinearRegression(fit_intercept = False)
+lin_mod.fit(x_train, y_train)
+print(lin_mod.intercept_)
+print(lin_mod.coef_)
+
+###### Evaluate the model ######
+def print_metrics(y_true, y_predicted, n_parameters):
+    ## First compute R^2 and the adjusted R^2
+    r2 = sklm.r2_score(y_true, y_predicted)
+    r2_adj = r2 - (n_parameters - 1)/(y_true.shape[0] - n_parameters) * (1 - r2)
+    
+    ## Print the usual metrics and the R^2 values
+    print('Mean Square Error      = ' + str(sklm.mean_squared_error(y_true, y_predicted)))
+    print('Root Mean Square Error = ' + str(math.sqrt(sklm.mean_squared_error(y_true, y_predicted))))
+    print('Mean Absolute Error    = ' + str(sklm.mean_absolute_error(y_true, y_predicted)))
+    print('Median Absolute Error  = ' + str(sklm.median_absolute_error(y_true, y_predicted)))
+    print('R^2                    = ' + str(r2))
+    print('Adjusted R^2           = ' + str(r2_adj))
+   
+y_score = lin_mod.predict(x_test) 
+print_metrics(y_test, y_score, 28)
+
+def print_metrics(y_true, y_predicted, n_parameters):
+    ## First compute R^2 and the adjusted R^2
+    r2 = sklm.r2_score(y_true, y_predicted)
+    r2_adj = r2 - (n_parameters - 1)/(y_true.shape[0] - n_parameters) * (1 - r2)
+    
+    ## Print the usual metrics and the R^2 values
+    print('Mean Square Error      = ' + str(sklm.mean_squared_error(y_true, y_predicted)))
+    print('Root Mean Square Error = ' + str(math.sqrt(sklm.mean_squared_error(y_true, y_predicted))))
+    print('Mean Absolute Error    = ' + str(sklm.mean_absolute_error(y_true, y_predicted)))
+    print('Median Absolute Error  = ' + str(sklm.median_absolute_error(y_true, y_predicted)))
+    print('R^2                    = ' + str(r2))
+    print('Adjusted R^2           = ' + str(r2_adj))
+   
+y_score = lin_mod.predict(x_test) 
+print_metrics(y_test, y_score, 28)
+
+
+def resid_qqresid_qq(y_test, y_score):
+    ## first compute vector of residuals. 
+    resids = np.subtract(y_test.reshape(-1,1), y_score.reshape(-1,1))
+    ## now make the residual plots
+    ss.probplot(resids.flatten(), plot = plt)
+    plt.title('Residuals vs. predicted values')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residual')
+    
+resid_qq(y_test, y_score)
+
+def resid_plot(y_test, y_score):
+    ## first compute vector of residuals. 
+    resids = np.subtract(y_test.reshape(-1,1), y_score.reshape(-1,1))
+    ## now make the residual plots
+    sns.regplot(y_score, resids, fit_reg=False)
+    plt.title('Residuals vs. predicted values')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residual')
+
+resid_plot(y_test, y_score)
+# transform the log price back to original price
+y_score_untransformy_score_  = np.exp(y_score)
+y_test_untransform = np.exp(y_test)
+resid_plot(y_test_untransform, y_score_untransform)
