4b1e4b6258ca1fbbe6dff4c991c0298b45cc2a99
stevenwangmx <wangmingxi7@Hotmail.com>
2018-08-17 15:08:22 -0400

    Update regularization.py

--- regularization.py
+++ regularization.py
@@ -11,8 +11,158 @@ import math
 
 %matplotlib inline
 
-
+###### Load prepared dataset ######
 Features = np.array(pd.read_csv('Auto_Data_Features.csv'))
 Labels = np.array(pd.read_csv('Auto_Data_Labels.csv'))
 print(Features.shape)
 print(Labels.shape)
+
+###### Split the dataset ######
+## Randomly sample cases to create independent training and test data
+nr.seed(9988)
+indx = range(Features.shape[0])
+indx = ms.train_test_split(indx, test_size = 40)
+
+x_train = Features[indx[0],:]
+y_train = np.ravel(Labels[indx[0]])
+x_test = Features[indx[1],:]
+y_test = np.ravel(Labels[indx[1]])
+
+###### Apply linear regression model without regularization ######
+## define and fit the linear regression model
+lin_mod = linear_model.LinearRegression()
+lin_mod.fit(x_train, y_train)
+
+def print_metrics(y_true, y_predicted):
+    ## First compute R^2 and the adjusted R^2
+    r2 = sklm.r2_score(y_true, y_predicted)
+    
+    ## Print the usual metrics and the R^2 values
+    print('Mean Square Error      = ' + str(sklm.mean_squared_error(y_true, y_predicted)))
+    print('Root Mean Square Error = ' + str(math.sqrt(sklm.mean_squared_error(y_true, y_predicted))))
+    print('Mean Absolute Error    = ' + str(sklm.mean_absolute_error(y_true, y_predicted)))
+    print('Median Absolute Error  = ' + str(sklm.median_absolute_error(y_true, y_predicted)))
+    print('R^2                    = ' + str(r2))
+    
+def resid_plot(y_test, y_score):
+    ## first compute vector of residuals. 
+    resids = np.subtract(y_test.reshape(-1,1), y_score.reshape(-1,1))
+    ## now make the residual plots
+    sns.regplot(y_score, resids, fit_reg=False)
+    plt.title('Residuals vs. predicted values')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residual')
+    plt.show()
+
+def hist_resids(y_test, y_score):
+    ## first compute vector of residuals. 
+    resids = np.subtract(y_test.reshape(-1,1), y_score.reshape(-1,1))
+    ## now make the residual plots
+    sns.distplot(resids)
+    plt.title('Histogram of residuals')
+    plt.xlabel('Residual value')
+    plt.ylabel('count')
+    plt.show()
+    
+def resid_qq(y_test, y_score):
+    ## first compute vector of residuals. 
+    resids = np.subtract(y_test, y_score)
+    ## now make the residual plots
+    ss.probplot(resids.flatten(), plot = plt)
+    plt.title('Residuals vs. predicted values')
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residual')
+    plt.show()
+  
+y_score = lin_mod.predict(x_test) 
+print_metrics(y_test, y_score) 
+hist_resids(y_test, y_score)  
+resid_qq(y_test, y_score) 
+resid_plot(y_test, y_score)
+
+###### Apply l2 (Ridge regression) regularization ######
+def plot_regularization(l, train_RMSE, test_RMSE, coefs, min_idx, title):   
+    plt.plot(l, test_RMSE, color = 'red', label = 'Test RMSE')
+    plt.plot(l, train_RMSE, label = 'Train RMSE')    
+    plt.axvline(min_idx, color = 'black', linestyle = '--')
+    plt.legend()
+    plt.xlabel('Regularization parameter')
+    plt.ylabel('Root Mean Square Error')
+    plt.title(title)
+    plt.show()
+    
+    plt.plot(l, coefs)
+    plt.axvline(min_idx, color = 'black', linestyle = '--')
+    plt.title('Model coefficient values \n vs. regularizaton parameter')
+    plt.xlabel('Regularization parameter')
+    plt.ylabel('Model coefficient value')
+    plt.show()
+
+def test_regularization_l2(x_train, y_train, x_test, y_test, l2):
+    train_RMSE = []
+    test_RMSE = []
+    coefs = []
+    for reg in l2:
+        lin_mod = linear_model.Ridge(alpha = reg)
+        lin_mod.fit(x_train, y_train)
+        coefs.append(lin_mod.coef_)
+        y_score_train = lin_mod.predict(x_train)
+        train_RMSE.append(sklm.mean_squared_error(y_train, y_score_train))
+        y_score = lin_mod.predict(x_test)
+        test_RMSE.append(sklm.mean_squared_error(y_test, y_score))
+    min_idx = np.argmin(test_RMSE)
+    min_l2 = l2[min_idx]
+    min_RMSE = test_RMSE[min_idx]
+    
+    title = 'Train and test root mean square error \n vs. regularization parameter'
+    plot_regularization(l2, train_RMSE, test_RMSE, coefs, min_l2, title)
+    return min_l2, min_RMSE
+     
+l2 = [x for x in range(1,101)]
+out_l2 = test_regularization_l2(x_train, y_train, x_test, y_test, l2)
+print(out_l2)  #display best l2 value
+
+## Evaluate the model using best l2 regularization
+lin_mod_l2 = linear_model.Ridge(alpha = out_l2[0])
+lin_mod_l2.fit(x_train, y_train)
+y_score_l2 = lin_mod_l2.predict(x_test)
+
+print_metrics(y_test, y_score_l2)
+hist_resids(y_test, y_score_l2)  
+resid_qq(y_test, y_score_l2) 
+resid_plot(y_test, y_score_l2) 
+
+###### Apply l1 (Lasso) regularization ######
+def test_regularization_l1(x_train, y_train, x_test, y_test, l1):
+    train_RMSE = []
+    test_RMSE = []
+    coefs = []
+    for reg in l1:
+        lin_mod = linear_model.Lasso(alpha = reg)
+        lin_mod.fit(x_train, y_train)
+        coefs.append(lin_mod.coef_)
+        y_score_train = lin_mod.predict(x_train)
+        train_RMSE.append(sklm.mean_squared_error(y_train, y_score_train))
+        y_score = lin_mod.predict(x_test)
+        test_RMSE.append(sklm.mean_squared_error(y_test, y_score))
+    min_idx = np.argmin(test_RMSE)
+    min_l1 = l1[min_idx]
+    min_RMSE = test_RMSE[min_idx]
+    
+    title = 'Train and test root mean square error \n vs. regularization parameter'
+    plot_regularization(l1, train_RMSE, test_RMSE, coefs, min_l1, title)
+    return min_l1, min_RMSE
+    
+l1 = [x/5000 for x in range(1,101)]
+out_l1 = test_regularization_l1(x_train, y_train, x_test, y_test, l1)
+print(out_l1)
+
+## Evaluate best l1 regularization
+lin_mod_l1 = linear_model.Lasso(alpha = out_l1[0])
+lin_mod_l1.fit(x_train, y_train)
+y_score_l1 = lin_mod_l1.predict(x_test)
+
+print_metrics(y_test, y_score_l1) 
+hist_resids(y_test, y_score_l1)  
+resid_qq(y_test, y_score_l1) 
+resid_plot(y_test, y_score_l1) 
