365835d5b8817f7ff35bead9d4db091807ef663d
stevenwangmx <wangmingxi7@Hotmail.com>
2018-08-17 12:35:47 -0400

    Update applyingLinearRegression.py

--- applyingLinearRegression.py
+++ applyingLinearRegression.py
@@ -11,3 +11,43 @@ import scipy.stats as ss
 import math
 
 %matplotlib inline
+
+###### Load prepared data set ######
+auto_prices = pd.read_csv('Auto_Data_Preped.csv')
+
+###### Prepare model matrix ######
+## Create dummy variables from categorical features using one hot encoding    ##
+## -- because scikit-learn models require numpy array of numeric ONLY values. ##
+
+## Encoding one feature
+print(auto_prices['body_style'].unique())   # display body_style before encoding
+Features = auto_prices['body_style']
+enc = preprocessing.LabelEncoder()
+enc.fit(Features)
+Features = enc.transform(Features)
+ohe = preprocessing.OneHotEncoder()
+encoded = ohe.fit(Features.reshape(-1,1))
+Features = encoded.transform(Features.reshape(-1,1)).toarray()
+## finished
+
+## use function to encode all other features left and append them together
+def encode_string(cat_feature):
+    ## First encode the strings to numeric categories
+    enc = preprocessing.LabelEncoder()
+    enc.fit(cat_feature)
+    enc_cat_feature = enc.transform(cat_feature)
+    ## Now, apply one hot encoding
+    ohe = preprocessing.OneHotEncoder()
+    encoded = ohe.fit(enc_cat_feature.reshape(-1,1))
+    return encoded.transform(enc_cat_feature.reshape(-1,1)).toarray()
+    
+categorical_columns = ['fuel_type', 'aspiration', 'drive_wheels', 'num_of_cylinders']
+
+for col in categorical_columns:
+    temp = encode_string(auto_prices[col])
+    Features = np.concatenate([Features, temp], axis = 1)
+
+print(Features.shape)   # display result
+print(Features[:2, :])  # display result  
+## finished
+
