92a7a991d68ce2844ac83f62f4008ce2f5e3e4cf
stevenwangmx <wangmingxi7@Hotmail.com>
2018-08-17 12:10:22 -0400

    Create applyingLinearRegression.py

---
+++ applyingLinearRegression.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn import preprocessing
+import sklearn.model_selection as ms
+from sklearn import linear_model
+import sklearn.metrics as sklm
+import numpy as np
+import numpy.random as nr
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as ss
+import math
+
+%matplotlib inline
