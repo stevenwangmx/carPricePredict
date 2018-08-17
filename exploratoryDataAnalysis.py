30c16551972399a08777302e200b7b092dcceba6
stevenwangmx <wangmingxi7@Hotmail.com>
2018-08-17 10:58:19 -0400

    Update exploratoryDataAnalysis.py

--- exploratoryDataAnalysis.py
+++ exploratoryDataAnalysis.py
@@ -5,3 +5,261 @@ import seaborn as sns
 
 %matplotlib inline
 
+###### STEP 1: Read the data set ######
+auto_prices = pd.read_csv('Automobile price data _Raw_.csv')
+
+###### STEP 2: Format data set ######
+def clean_auto_data(auto_prices):
+    import pandas as pd
+    import numpy as np
+    
+    ## Recode names
+    ## fix column names so the '-' character becomes '_'
+    cols = auto_prices.columns
+    auto_prices.columns = [str.replace('-', '_') for str in cols]
+    
+    ## Treat missing values
+    ## Remove rows with missing values, accounting for mising values coded as '?'
+    cols = ['price', 'bore', 'stroke', 'horsepower', 'peak_rpm']
+    for column in cols:
+        auto_prices.loc[auto_prices[column] == '?', column] = np.nan
+    auto_prices.dropna(axis = 0, inplace = True)
+
+    ## Transform column data type
+    ## Convert some columns to numeric values
+    for column in cols:
+        auto_prices[column] = pd.to_numeric(auto_prices[column])
+
+    return auto_prices
+  
+auto_prices = clean_auto_data(auto_prices)
+
+## display the types of the data set
+auto_prices.dtypes
+
+## display summary statistics for numeric columns
+auto_prices.describe()
+
+###### STEP 3: Display frenquency table ###### -- to understand the distributions of categorical variables
+def count_unique(auto_prices, cols):
+    for col in cols:
+        print('\n' + 'For column ' + col)
+        print(auto_prices[col].value_counts())
+
+cat_cols = ['make', 'fuel_type', 'aspiration', 'num_of_doors', 'body_style', 
+            'drive_wheels', 'engine_location', 'engine_type', 'num_of_cylinders', 
+            'fuel_system']
+count_unique(auto_prices, cat_cols)
+
+###### STEP 4: Visualzing distributions ######
+## Bar charts
+def plot_bars(auto_prices, cols):
+    for col in cols:
+        fig = plt.figure(figsize=(6,6)) # define plot area
+        ax = fig.gca() # define axis    
+        counts = auto_prices[col].value_counts() # find the counts for each unique category
+        counts.plot.bar(ax = ax, color = 'blue') # Use the plot.bar method on the counts data frame
+        ax.set_title('Number of autos by' + col) # Give the plot a main title
+        ax.set_xlabel(col) # Set text for the x axis
+        ax.set_ylabel('Number of autos')# Set text for y axis
+        plt.show()
+
+plot_cols = ['make', 'body_style', 'num_of_cylinders']
+plot_bars(auto_prices, plot_cols)    
+
+## Historgrams
+def plot_histogram(auto_prices, cols, bins = 10):
+    for col in cols:
+        fig = plt.figure(figsize=(6,6)) # define plot area
+        ax = fig.gca() # define axis    
+        auto_prices[col].plot.hist(ax = ax, bins = bins) # Use the plot.hist method on subset of the data frame
+        ax.set_title('Histogram of ' + col) # Give the plot a main title
+        ax.set_xlabel(col) # Set text for the x axis
+        ax.set_ylabel('Number of autos')# Set text for y axis
+        plt.show()
+        
+num_cols = ['curb_weight', 'engine_size', 'city_mpg', 'price']    
+plot_histogram(auto_prices, num_cols)
+
+## Kernel density estimation --KDE
+def plot_density_hist(auto_pries, cols, bins = 10, hist = False):
+    for col in cols:
+        sns.set_style("whitegrid")
+        sns.distplot(auto_prices[col], bins = bins, rug=True, hist = hist)
+        plt.title('Histogram of ' + col) # Give the plot a main title
+        plt.xlabel(col) # Set text for the x axis
+        plt.ylabel('Number of autos')# Set text for y axis
+        plt.show()
+        
+plot_density_hist(auto_prices, num_cols)   
+## combine kde and histogram
+plot_density_hist(auto_prices, num_cols, bins = 20, hist = True) 
+
+## Two dimensional plot for numeric varibles: shows relationship of two variables 
+## Scatter plot
+def plot_scatter(auto_prices, cols, col_y = 'price'):
+    for col in cols:
+        fig = plt.figure(figsize=(7,6)) # define plot area
+        ax = fig.gca() # define axis   
+        auto_prices.plot.scatter(x = col, y = col_y, ax = ax)
+        ax.set_title('Scatter plot of ' + col_y + ' vs. ' + col) # Give the plot a main title
+        ax.set_xlabel(col) # Set text for the x axis
+        ax.set_ylabel(col_y)# Set text for y axis
+        plt.show()
+
+num_cols = ['curb_weight', 'engine_size', 'horsepower', 'city_mpg']
+plot_scatter(auto_prices, num_cols)        
+
+## Scatter plot with transparency to deal with over plotting
+def plot_scatter_t(auto_prices, cols, col_y = 'price', alpha = 1.0):
+    for col in cols:
+        fig = plt.figure(figsize=(7,6)) # define plot area
+        ax = fig.gca() # define axis   
+        auto_prices.plot.scatter(x = col, y = col_y, ax = ax, alpha = alpha)
+        ax.set_title('Scatter plot of ' + col_y + ' vs. ' + col) # Give the plot a main title
+        ax.set_xlabel(col) # Set text for the x axis
+        ax.set_ylabel(col_y)# Set text for y axis
+        plt.show()
+
+plot_scatter_t(auto_prices, num_cols, alpha = 0.2)     
+
+## 2d density plots to deal with over plotting
+def plot_desity_2d(auto_prices, cols, col_y = 'price', kind ='kde'):
+    for col in cols:
+        sns.set_style("whitegrid")
+        sns.jointplot(col, col_y, data=auto_prices, kind=kind)
+        plt.xlabel(col) # Set text for the x axis
+        plt.ylabel(col_y)# Set text for y axis
+        plt.show()
+
+plot_desity_2d(auto_prices, num_cols)   
+## hexbin plots for the same value
+plot_desity_2d(auto_prices, num_cols, kind = 'hex') 
+
+## Two dimensional for categorical and numeric variables
+## Box plots
+def plot_box(auto_prices, cols, col_y = 'price'):
+    for col in cols:
+        sns.set_style("whitegrid")
+        sns.boxplot(col, col_y, data=auto_prices)
+        plt.xlabel(col) # Set text for the x axis
+        plt.ylabel(col_y)# Set text for y axis
+        plt.show()
+        
+cat_cols = ['fuel_type', 'aspiration', 'num_of_doors', 'body_style', 
+            'drive_wheels', 'engine_location', 'engine_type', 'num_of_cylinders']
+plot_box(auto_prices, cat_cols) 
+
+## Violine plots
+def plot_violin(auto_prices, cols, col_y = 'price'):
+    for col in cols:
+        sns.set_style("whitegrid")
+        sns.violinplot(col, col_y, data=auto_prices)
+        plt.xlabel(col) # Set text for the x axis
+        plt.ylabel(col_y)# Set text for y axis
+        plt.show()
+        
+plot_violin(auto_prices, cat_cols)
+
+## Multi dimensional plots 
+## Marker shape
+def plot_scatter_shape(auto_prices, cols, shape_col = 'fuel_type', col_y = 'price', alpha = 0.2):
+    shapes = ['+', 'o', 's', 'x', '^'] # pick distinctive shapes
+    unique_cats = auto_prices[shape_col].unique()
+    for col in cols: # loop over the columns to plot
+        sns.set_style("whitegrid")
+        for i, cat in enumerate(unique_cats): # loop over the unique categories
+            temp = auto_prices[auto_prices[shape_col] == cat]
+            sns.regplot(col, col_y, data=temp, marker = shapes[i], label = cat,
+                        scatter_kws={"alpha":alpha}, fit_reg = False, color = 'blue')
+        plt.title('Scatter plot of ' + col_y + ' vs. ' + col) # Give the plot a main title
+        plt.xlabel(col) # Set text for the x axis
+        plt.ylabel(col_y)# Set text for y axis
+        plt.legend()
+        plt.show()
+            
+num_cols = ['curb_weight', 'engine_size', 'horsepower', 'city_mpg']
+plot_scatter_shape(auto_prices, num_cols)    
+
+## Marker size
+def plot_scatter_size(auto_prices, cols, shape_col = 'fuel_type', size_col = 'curb_weight',
+                            size_mul = 0.000025, col_y = 'price', alpha = 0.2):
+    shapes = ['+', 'o', 's', 'x', '^'] # pick distinctive shapes
+    unique_cats = auto_prices[shape_col].unique()
+    for col in cols: # loop over the columns to plot
+        sns.set_style("whitegrid")
+        for i, cat in enumerate(unique_cats): # loop over the unique categories
+            temp = auto_prices[auto_prices[shape_col] == cat]
+            sns.regplot(col, col_y, data=temp, marker = shapes[i], label = cat,
+                        scatter_kws={"alpha":alpha, "s":size_mul*auto_prices[size_col]**2}, 
+                        fit_reg = False, color = 'blue')
+        plt.title('Scatter plot of ' + col_y + ' vs. ' + col) # Give the plot a main title
+        plt.xlabel(col) # Set text for the x axis
+        plt.ylabel(col_y)# Set text for y axis
+        plt.legend()
+        plt.show()
+
+num_cols = ['engine_size', 'horsepower', 'city_mpg']
+plot_scatter_size(auto_prices, num_cols)  
+
+## Colour
+def plot_scatter_shape_size_col(auto_prices, cols, shape_col = 'fuel_type', size_col = 'curb_weight',
+                            size_mul = 0.000025, color_col = 'aspiration', col_y = 'price', alpha = 0.2):
+    shapes = ['+', 'o', 's', 'x', '^'] # pick distinctive shapes
+    colors = ['green', 'blue', 'orange', 'magenta', 'gray'] # specify distinctive colors
+    unique_cats = auto_prices[shape_col].unique()
+    unique_colors = auto_prices[color_col].unique()
+    for col in cols: # loop over the columns to plot
+        sns.set_style("whitegrid")
+        for i, cat in enumerate(unique_cats): # loop over the unique categories
+            for j, color in enumerate(unique_colors):
+                temp = auto_prices[(auto_prices[shape_col] == cat) & (auto_prices[color_col] == color)]
+                sns.regplot(col, col_y, data=temp, marker = shapes[i],
+                            scatter_kws={"alpha":alpha, "s":size_mul*temp[size_col]**2}, 
+                            label = (cat + ' and ' + color), fit_reg = False, color = colors[j])
+        plt.title('Scatter plot of ' + col_y + ' vs. ' + col) # Give the plot a main title
+        plt.xlabel(col) # Set text for the x axis
+        plt.ylabel(col_y)# Set text for y axis
+        plt.legend()
+        plt.show()
+
+num_cols = ['engine_size', 'horsepower', 'city_mpg']        
+plot_scatter_shape_size_col(auto_prices, num_cols)     
+
+## Multi axis views of data
+## Pair-wise scatter plot --an array of scatter plots with common axes along the rows and columns of the array
+num_cols = ["curb_weight", "engine_size", "horsepower", "city_mpg", "price", "fuel_type"] 
+sns.pairplot(auto_prices[num_cols], hue='fuel_type', palette="Set2", diag_kind="kde", size=2).map_upper(sns.kdeplot, cmap="Blues_d")
+
+## Conditioned plot --use group-by operations to create and display subsets of the dataset.
+  ## Function to plot conditioned histograms
+def cond_hists(df, plot_cols, grid_col):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    ## Loop over the list of columns
+    for col in plot_cols:
+        grid1 = sns.FacetGrid(df, col=grid_col)
+        grid1.map(plt.hist, col, alpha=.7)
+    return grid_col
+
+  ## Define columns for making a conditioned histogram
+plot_cols2 = ["length",
+               "curb_weight",
+               "engine_size",
+               "city_mpg",
+               "price"]
+
+cond_hists(auto_prices, plot_cols2, 'drive_wheels')
+
+##two dimensional array of scatter plots
+def cond_plot(cols):
+    import IPython.html.widgets
+    import seaborn as sns
+    for col in cols:
+        g = sns.FacetGrid(auto_prices, col="drive_wheels", row = 'body_style', 
+                      hue="fuel_type", palette="Set2", margin_titles=True)
+        g.map(sns.regplot, col, "price", fit_reg = False)
+
+num_cols = ["curb_weight", "engine_size", "city_mpg"]
+cond_plot(num_cols)    
+
