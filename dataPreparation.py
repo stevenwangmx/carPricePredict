ef304db6bee524645a86712fb34ecbe5543d5eb0
stevenwangmx <wangmingxi7@Hotmail.com>
2018-08-17 11:46:47 -0400

    Update dataPreparation.py

--- dataPreparation.py
+++ dataPreparation.py
@@ -7,5 +7,100 @@ import math
 
 %matplotlib inline
 
+###### Load data set ######
 auto_prices = pd.read_csv('Automobile price data _Raw_.csv')
 auto_prices.head(20)
+
+###### Recode names ######
+auto_prices.columns = [str.replace('-', '_') for str in auto_prices.columns]
+
+###### Treat missing values ######
+(auto_prices.astype(np.object) == '?').any()
+auto_prices.dtypes
+for col in auto_prices.columns:
+    if auto_prices[col].dtype == object:
+        count = 0
+        count = [count + 1 for x in auto_prices[col] if x == '?']
+        print(col + ' ' + str(sum(count)))
+## Drop column with too many missing values
+auto_prices.drop('normalized_losses', axis = 1, inplace = True)
+## Remove rows with missing values, accounting for mising values coded as '?'
+cols = ['price', 'bore', 'stroke',
+          'horsepower', 'peak_rpm']
+for column in cols:
+    auto_prices.loc[auto_prices[column] == '?', column] = np.nan
+    auto_prices.dropna(axis = 0, inplace = True)
+auto_prices.shape    
+
+###### Transform column data type ######
+for column in cols:
+    auto_prices[column] = pd.to_numeric(auto_prices[column])
+auto_prices[cols].dtypes
+
+###### Feature engineering and transforming variables ######
+#### 1. Aggregating categorical variables ####
+auto_prices['num_of_cylinders'].value_counts()
+cylinder_categories = {'three':'three_four', 'four':'three_four', 
+                    'five':'five_six', 'six':'five_six',
+                    'eight':'eight_twelve', 'twelve':'eight_twelve'}
+auto_prices['num_of_cylinders'] = [cylinder_categories[x] for x in auto_prices['num_of_cylinders']]
+auto_prices['num_of_cylinders'].value_counts()
+
+def plot_box(auto_prices, col, col_y = 'price'):  # create plot to examine the result
+    sns.set_style("whitegrid")
+    sns.boxplot(col, col_y, data=auto_prices)
+    plt.xlabel(col) # Set text for the x axis
+    plt.ylabel(col_y)# Set text for y axis
+    plt.show()
+    
+plot_box(auto_prices, 'num_of_cylinders')     
+
+auto_prices['body_style'].value_counts()
+body_cats = {'sedan':'sedan', 'hatchback':'hatchback', 'wagon':'wagon', 
+             'hardtop':'hardtop_convert', 'convertible':'hardtop_convert'}
+auto_prices['body_style'] = [body_cats[x] for x in auto_prices['body_style']]
+auto_prices['body_style'].value_counts()
+
+def plot_box(auto_prices, col, col_y = 'price'):  # create plot to examine the result
+    sns.set_style("whitegrid")
+    sns.boxplot(col, col_y, data=auto_prices)
+    plt.xlabel(col) # Set text for the x axis
+    plt.ylabel(col_y)# Set text for y axis
+    plt.show()
+    
+plot_box(auto_prices, 'body_style')   
+
+#### 2. Transforming numeric variables ####
+def hist_plot(vals, lab):
+    ## Distribution plot of values
+    sns.distplot(vals)
+    plt.title('Histogram of ' + lab)
+    plt.xlabel('Value')
+    plt.ylabel('Density')
+    
+#labels = np.array(auto_prices['price'])
+hist_plot(auto_prices['price'], 'prices')
+auto_prices['log_price'] = np.log(auto_prices['price'])  # use logarithm to centralize left skewed distribution
+hist_plot(auto_prices['log_price'], 'log prices')
+
+#check how the logarithm changed the relationship between labels and features
+def plot_scatter_shape(auto_prices, cols, shape_col = 'fuel_type', col_y = 'log_price', alpha = 0.2):
+    shapes = ['+', 'o', 's', 'x', '^'] # pick distinctive shapes
+    unique_cats = auto_prices[shape_col].unique()
+    for col in cols: # loop over the columns to plot
+        sns.set_style("whitegrid")
+        for i, cat in enumerate(unique_cats): # loop over the unique categories
+            temp = auto_prices[auto_prices[shape_col] == cat]
+            sns.regplot(col, col_y, data=temp, marker = shapes[i], label = cat,
+                        scatter_kws={"alpha":alpha}, fit_reg = False, color = 'blue')
+        plt.title('Scatter plot of ' + col_y + ' vs. ' + col) # Give the plot a main title
+        plt.xlabel(col) # Set text for the x axis
+        plt.ylabel(col_y)# Set text for y axis
+        plt.legend()
+        plt.show()
+            
+num_cols = ['curb_weight', 'engine_size', 'horsepower', 'city_mpg']
+plot_scatter_shape(auto_prices, num_cols)  
+
+###### Save dataframe to file ######
+auto_prices.to_csv('Auto_Data_Preped.csv', index = False, header = True)
